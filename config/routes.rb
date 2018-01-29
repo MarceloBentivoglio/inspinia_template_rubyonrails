0fc258e05a242f7b7f7854574b60fd31eba393bd
Marcelo Bentivoglio <marcelo.bentivoglio@mvpinvest.com.br>
2018-01-29 11:44:49 -0200

    added a route and show of seller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :invoices, only: [:new, :create, :show]
-  resources :sellers, only: [:new, :create]
+  resources :sellers, only: [:new, :create, :show]
   resources :payers, only: [:new, :create]
   resources :operations, only: [:new, :show]
 
