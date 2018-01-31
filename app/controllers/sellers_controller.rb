ac2a5e866e954da8384a78f88b2a206f5b5f6932
Marcelo Bentivoglio <marcelo.bentivoglio@mvpinvest.com.br>
2018-01-31 18:31:50 -0200

    added chartkick and groupdate gems

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -24,26 +24,26 @@ class SellersController < ApplicationController
     @revenue = extract_revenue_in_integer(year_revenue)
     #TODO get the legal from the year that the user wants
     @year_legal = @seller.legals.first
-
-
   end
 
   private
 
   def extract_revenue_in_integer(revenue_object)
     revenue_array = []
-    revenue_array.push(revenue_object.jan.to_s.to_i)
-    revenue_array.push(revenue_object.feb.to_s.to_i)
-    revenue_array.push(revenue_object.mar.to_s.to_i)
-    revenue_array.push(revenue_object.apr.to_s.to_i)
-    revenue_array.push(revenue_object.may.to_s.to_i)
-    revenue_array.push(revenue_object.jun.to_s.to_i)
-    revenue_array.push(revenue_object.jul.to_s.to_i)
-    revenue_array.push(revenue_object.aug.to_s.to_i)
-    revenue_array.push(revenue_object.sep.to_s.to_i)
-    revenue_array.push(revenue_object.oct.to_s.to_i)
-    revenue_array.push(revenue_object.nov.to_s.to_i)
-    revenue_array.push(revenue_object.dec.to_s.to_i)
+    revenue_array.push(["Jan", revenue_object.jan.to_s.to_i])
+    revenue_array.push(["Fev", revenue_object.feb.to_s.to_i])
+    revenue_array.push(["Mar", revenue_object.mar.to_s.to_i])
+    revenue_array.push(["Abr", revenue_object.apr.to_s.to_i])
+    revenue_array.push(["Mai", revenue_object.may.to_s.to_i])
+    revenue_array.push(["Jun", revenue_object.jun.to_s.to_i])
+    revenue_array.push(["Jul", revenue_object.jul.to_s.to_i])
+    revenue_array.push(["Ago", revenue_object.aug.to_s.to_i])
+    revenue_array.push(["Set", revenue_object.sep.to_s.to_i])
+    revenue_array.push(["Out", revenue_object.oct.to_s.to_i])
+    revenue_array.push(["Nov", revenue_object.nov.to_s.to_i])
+    revenue_array.push(["Dez", revenue_object.dec.to_s.to_i])
+
+
   end
 
   def seller_params
