6dbfe44fe202d2943b77a133013ad1e455014008
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-02-19 18:58:58 -0300

    primeiras adaptações para trabalharmos nos payer

--- app/controllers/payers_controller.rb
+++ app/controllers/payers_controller.rb
@@ -13,6 +13,17 @@ class PayersController < ApplicationController
     end
   end
 
+  def show
+    @payer = Payer.find(params[:id])
+    #TODO get the data from the year that the user wants. in fact the description should be on the payer not in the finantital
+    # @finantial = @payer.finantials.first
+
+    # @total_operations_number = @payer.operations.count
+    # @total_operations_amount = Money.new(@payer.operations.sum('total_value_cents'))
+    # @operations_report = @payer.operations.group_by_month(:deposit_date, format: "%b %y").sum("total_value_cents")
+    # @operations_report.transform_values! {|monthly_operations_amount| monthly_operations_amount.to_f/100}
+  end
+
   private
 
   def payer_params
