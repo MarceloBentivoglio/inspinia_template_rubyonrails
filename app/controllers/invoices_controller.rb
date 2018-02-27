a6b7707b43d85b6f91441acf4f4826cd49c9c49c
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-02-27 17:03:22 -0300

    bloco da direita com stacked graphs

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -19,8 +19,28 @@ class InvoicesController < ApplicationController
     @payer = @invoice.payer
     @installments = @invoice.installments
 
-    @operations_report = @seller.operations.group_by_month(:deposit_date, last: 8, format: "%b %y").sum("total_value_cents")
-    @operations_report.transform_values! {|monthly_operations_amount| monthly_operations_amount.to_f/100}
+    @installments_paid_amount = Money.new(@seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: true}}).sum("value_cents"))
+    @installments_paid_quantity = @seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: true}}).count
+    @installments_paid = @seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: true}}).group_by_month(:deposit_date, last: 8, format: "%b %y").sum("value_cents")
+    @installments_paid.transform_values! {|monthly_amount| monthly_amount.to_f/100}
+
+    time_range_future = Time.now..(Time.now + 1.year)
+    @installments_on_date_amount = Money.new(@seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: false, due_date: time_range_future}}).sum("value_cents"))
+    @installments_on_date_quantity = @seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: false, due_date: time_range_future}}).count
+    @installments_on_date = @seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: false, due_date: time_range_future}}).group_by_month(:deposit_date, last: 8, format: "%b %y").sum("value_cents")
+    @installments_on_date.transform_values! {|monthly_amount| monthly_amount.to_f/100}
+
+    time_range_past = (Time.now - 6.years)...(Time.now)
+    @installments_overdue_amount = Money.new(@seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: false, due_date: time_range_past}}).sum("value_cents"))
+    @installments_overdue_quantity = @seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: false, due_date: time_range_past}}).count
+    @installments_overdue = @seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: false, due_date: time_range_past}}).group_by_month(:deposit_date, last: 8, format: "%b %y").sum("value_cents")
+    @installments_overdue.transform_values! {|monthly_amount| monthly_amount.to_f/100}
+
+    @installments_all_keys = @installments_overdue.merge(@installments_on_date.merge(@installments_paid))
+    @installments_all_keys = @installments_all_keys.sort_by {|date, value| Date.strptime(date, "%b %y") }.to_h
+    @installments_paid = merge_smaller_hash_into_bigger(@installments_paid, @installments_all_keys)
+    @installments_on_date = merge_smaller_hash_into_bigger(@installments_on_date, @installments_all_keys)
+    @installments_overdue = merge_smaller_hash_into_bigger(@installments_overdue, @installments_all_keys)
 
     if request.xhr?
       render layout: false
@@ -79,6 +99,14 @@ class InvoicesController < ApplicationController
   end
 
   private
+
+  def merge_smaller_hash_into_bigger(small, big)
+    new_hash = {}
+    big.each do |key, value|
+      new_hash[key] = small[key] ? small[key] : 0
+    end
+    return new_hash
+  end
 
   def invoice_and_installments_params
     # In the strong parameters we need to pass the attributes of intallments so that the invoice form can understand it
