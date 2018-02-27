12e90c9600cce623447dd4a31fa05907d9f2620d
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-02-27 15:36:29 -0300

    stacked graphs for clients

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -58,14 +58,22 @@ class SellersController < ApplicationController
     @total_operations_number = @seller.operations.count
     @total_operations_amount = Money.new(@seller.operations.sum('total_value_cents'))
 
+
+
+    @installments_paid_amount = Money.new(@seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: true}}).sum("value_cents"))
+    @installments_paid_quantity = @seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: true}}).count
     @installments_paid = @seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: true}}).group_by_month(:deposit_date, format: "%b %y").sum("value_cents")
     @installments_paid.transform_values! {|monthly_amount| monthly_amount.to_f/100}
 
     time_range_future = Time.now..(Time.now + 1.year)
+    @installments_on_date_amount = Money.new(@seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: false, due_date: time_range_future}}).sum("value_cents"))
+    @installments_on_date_quantity = @seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: false, due_date: time_range_future}}).count
     @installments_on_date = @seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: false, due_date: time_range_future}}).group_by_month(:deposit_date, format: "%b %y").sum("value_cents")
     @installments_on_date.transform_values! {|monthly_amount| monthly_amount.to_f/100}
 
     time_range_past = (Time.now - 6.years)...(Time.now)
+    @installments_overdue_amount = Money.new(@seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: false, due_date: time_range_past}}).sum("value_cents"))
+    @installments_overdue_quantity = @seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: false, due_date: time_range_past}}).count
     @installments_overdue = @seller.operations.joins(invoices: :installments).where(invoices: {installments: {paid: false, due_date: time_range_past}}).group_by_month(:deposit_date, format: "%b %y").sum("value_cents")
     @installments_overdue.transform_values! {|monthly_amount| monthly_amount.to_f/100}
 
