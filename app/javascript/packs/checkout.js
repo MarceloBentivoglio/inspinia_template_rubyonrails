0fee7a8e0849b9eba0b84c4cad34a5a4849a1211
Marcelo Bentivoglio <marcelo.bentivoglio@mvpinvest.com.br>
2018-02-27 10:26:59 -0300

    javascript working for checkout

--- app/javascript/packs/checkout.js
+++ app/javascript/packs/checkout.js
@@ -3,11 +3,11 @@ import currencyFormatter from 'currency-formatter';
 $("[data-invoice]").change(() => {
   const invoices = $("[data-invoice]:checked").toArray().map(el => JSON.parse(el.dataset.invoice));
   const total = invoices.reduce((sum, invoice) => sum + invoice.total_value_cents, 0);
-  const interest = invoices.reduce((sum, invoice) => sum + invoice.average_interest_cents, 0);
-  const ad_valorem = invoices.reduce((sum, invoice) => sum + invoice.average_ad_valorem_cents, 0);
-  const iof = invoices.reduce((sum, invoice) => sum + invoice.iof, 0);
-  const iof_ad = invoices.reduce((sum, invoice) => sum + invoice.iof_ad, 0);
-  const deposit_value = invoices.reduce((sum, invoice) => sum + invoice.deposit_value, 0);
+  const interest = invoices.reduce((sum, invoice) => sum + invoice.interest.fractional, 0);
+  const ad_valorem = invoices.reduce((sum, invoice) => sum + invoice.ad_valorem.fractional, 0);
+  const iof = invoices.reduce((sum, invoice) => sum + invoice.iof.fractional, 0);
+  const iof_ad = invoices.reduce((sum, invoice) => sum + invoice.iof_ad.fractional, 0);
+  const deposit_value = invoices.reduce((sum, invoice) => sum + invoice.deposit_value.fractional, 0);
 
   $("#total").text(format(total));
   $("#interest").text(format(interest));
