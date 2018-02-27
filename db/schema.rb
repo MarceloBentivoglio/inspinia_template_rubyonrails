bd928e078b21d8fd5afbb0c72ab5a1345cc105e3
Marcelo Bentivoglio <marcelo.bentivoglio@mvpinvest.com.br>
2018-02-27 15:00:23 -0300

    starting Admin Interface

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227143909) do
+ActiveRecord::Schema.define(version: 20180227175403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -417,6 +417,7 @@ ActiveRecord::Schema.define(version: 20180227143909) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "client_id"
+    t.boolean "admin", default: false, null: false
     t.index ["client_id"], name: "index_users_on_client_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
