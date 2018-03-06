260068a9e35fbfcea569a49ee75e46d9e1129579
Marcelo Bentivoglio <marcelo.bentivoglio@mvpinvest.com.br>
2018-03-06 10:38:45 -0300

    minor changes in email config

--- config/initializers/smtp.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: 'gmail.com',
-  user_name: ENV['mabj90s@gmail.com'],
-  password: ENV['040790Mabj!'],
-  authentication: :login,
-  enable_starttls_auto: true
-}
