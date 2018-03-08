f4047895a08d7135eec3146c8061aa7c9ccce2dd
Marcelo Bentivoglio <marcelo.bentivoglio@mvpinvest.com.br>
2018-03-08 14:24:47 -0300

    adjusted admin view of installments, invoices, payers and sellers

--- app/admin/payers.rb
+++ app/admin/payers.rb
@@ -1,15 +1,14 @@
 ActiveAdmin.register Payer do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
+  index do
+    selectable_column
+    column :identification_number
+    column :company_nickname
+    column :address
+    column :zip_code
+    column :city
+    column :email
+    column :phone_number
+    actions
+  end
 end
