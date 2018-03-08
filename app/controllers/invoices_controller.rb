f92192f0cca2f8f5410727c61b734f9fe6ad8dc0
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-03-08 19:04:01 -0300

    sold only for clients and deposit date

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,9 +1,9 @@
 class InvoicesController < ApplicationController
   def index
-    invoices_for_sale = Invoice.for_sale.includes(operation: :seller).to_a
+    invoices_for_sale = Invoice.for_sale.includes(:seller).to_a
     @rejected_invoices = Rejection.includes(:invoice).where(rejector: current_user.client).map(&:invoice)
     @offered_invoices = invoices_for_sale - @rejected_invoices
-    @purchased_invoices = Invoice.sold.includes(operation: :seller).where(buyer: current_user.client)
+    @purchased_invoices = Invoice.sold.joins(:client).where(buyer: current_user.client)
     # TODO: verificar se há melhora de performance com a linha de código abaixo
     # @purchased_invoices = Invoice.sold.includes(operation: {seller: {client: :user}}).where(buyer: current_user.client)
   end
