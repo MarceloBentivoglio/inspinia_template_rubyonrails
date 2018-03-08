675de4e691292d91a53cfb0363cc6af3cafbd0ee
Marcelo Bentivoglio <marcelo.bentivoglio@mvpinvest.com.br>
2018-03-08 18:34:05 -0300

    added 3 days to outstanding days

--- app/models/installment.rb
+++ app/models/installment.rb
@@ -16,7 +16,7 @@ class Installment < ApplicationRecord
   end
 
   def outstanding_days
-    due_date > Time.now ? TimeDifference.between(due_date, Time.now).in_days : 0
+    due_date > Time.now ? (TimeDifference.between(due_date, Time.now).in_days + 3) : 0
   end
 
   def closure_outstanding_days
