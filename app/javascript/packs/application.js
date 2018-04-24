7b2f7646d3f40eb44d9693dea419a80b255e6b95
Marcelo Bentivoglio <mabj90s@gmail.com>
2018-04-24 14:58:25 -0300

    datepicker invoice new

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import 'bootstrap';
 import "./invoices";
 import "./checkout";
 import "./invoices_new";
+import "./datepicker";
